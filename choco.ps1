cacf7f5fb040c7d57ea7ec4e31b22c349e7cc987
George <shalva258@gmail.com>
2021-05-28 13:39:09 +0400

    added few packages

---
+++ choco.ps1
@@ -0,0 +1,10 @@
+# Desktop
+choco install -y obsidian
+choco install -y steam-client
+choco install -y bitwarden
+
+# Development
+choco install -y openjdk11
+choco install -y python3
+choco install -y firacodenf
+choco install -y git
