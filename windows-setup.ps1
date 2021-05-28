98df8def07706717b9f8099a1ab4c49d0d82d29e
George <shalva258@gmail.com>
2021-05-28 14:09:19 +0400

    cleanup

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -9,3 +9,8 @@ choco install -y python3
 choco install -y firacodenf
 choco install -y git
 choco install -y vscode
+
+# Import Registry files in '.\REG' folder
+foreach ($reg in (Get-ChildItem -Path ".\REG\*" -Include @("*.reg")).FullName) {
+    reg.exe import $reg
+}
