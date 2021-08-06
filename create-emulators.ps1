b26cd8fc04afbb1efa5cb6a256375704a8c59e30
shalva97 <shalva258@gmail.com>
2021-08-06 23:03:42 +0400

    added system image download

--- create-emulators.ps1
+++ create-emulators.ps1
@@ -8,8 +8,12 @@ if (-Not (Test-Path $env:ANDROID_SDK_ROOT)) {
 $tools = $env:ANDROID_SDK_ROOT + "\tools\bin"
 $env:Path += ";" + $tools
 
+sdkmanager --install "system-images;android-30;default;x86"
+sdkmanager --install "system-images;android-24;default;x86"
+sdkmanager --install "system-images;android-27;default;x86"
+
 avdmanager create avd -n "API-24_Nougat_7.0_x86" -k "system-images;android-24;google_apis_playstore;x86" -d 8
 avdmanager create avd -n "API-27_Oreo_8.1.0_x86" -k "system-images;android-27;google_apis_playstore;x86" -d 8
 avdmanager create avd -n "API-30_Android11_11_x86" -k "system-images;android-30;google_apis_playstore;x86" -d 8
 
-# todo somehow download system images
+# todo somehow set hw.keyboard=yes to enables keys from your laptop to be sent to the emulator
