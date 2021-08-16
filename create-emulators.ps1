b008cb7b52aae7a44ed839c783f426bb1c406f58
shalva97 <shalva258@gmail.com>
2021-08-16 22:13:04 +0400

    switch to 64bit emulators

--- create-emulators.ps1
+++ create-emulators.ps1
@@ -8,13 +8,14 @@ if (-Not (Test-Path $env:ANDROID_SDK_ROOT)) {
 $tools = $env:ANDROID_SDK_ROOT + "\tools\bin"
 $env:Path += ";" + $tools
 
-sdkmanager --install "system-images;android-30;default;x86"
-sdkmanager --install "system-images;android-24;default;x86"
-sdkmanager --install "system-images;android-27;default;x86"
+# list available combinations:
+#     sdkmanager.bat --list
+sdkmanager --install "system-images;android-24;google_apis;x86_64"
+sdkmanager --install "system-images;android-28;google_apis_playstore;x86_64"
+sdkmanager --install "system-images;android-30;google_apis_playstore;x86_64"
 
-avdmanager create avd -n "API-24_Nougat_7.0_x86" -k "system-images;android-24;google_apis_playstore;x86" -d 8
-avdmanager create avd -n "API-27_Oreo_8.1.0_x86" -k "system-images;android-27;google_apis_playstore;x86" -d 8
-avdmanager create avd -n "API-30_Android11_11_x86" -k "system-images;android-30;google_apis_playstore;x86" -d 8
+avdmanager create avd --name "API-24_Nougat_7.0_x86-64" --package "system-images;android-24;google_apis;x86_64" --device 8
+avdmanager create avd --name "API-27_Pie_9.0_x86-64" --package "system-images;android-28;google_apis_playstore;x86_64" --device 8
+avdmanager create avd --name "API-30_Android11_11_x86-64" --package "system-images;android-30;google_apis_playstore;x86_64" --device 8
 
 # todo somehow set hw.keyboard=yes to enables keys from your laptop to be sent to the emulator
-# todo echo no | sdkmanager --install 'system-images;android-29;google_apis;x86_64'
