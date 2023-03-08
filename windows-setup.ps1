9cbdb66765fafa93f1a6b23ce3a841e0216937cc
George Shalvashvili <shalva258@gmail.com>
2023-03-08 23:40:33 +0400

    Update windows-setup.ps1

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -1,34 +1,17 @@
 ### Desktop
-# todo: install firacode
-winget install --id VideoLAN.VLC
-winget install --id Flameshot.Flameshot
-winget install --id KDE.KDEConnect
-winget install --id Opera.Opera
-winget install --id SlackTechnologies.Slack
-winget install --id Microsoft.PowerToys
-# winget install --id KDE.Kdenlive
-# winget install steam
+winget install VideoLAN.VLC
+winget install Flameshot.Flameshot
+winget install Alacritty.Alacritty
+winget install Microsoft.PowerToys
+winget install valve.steam
+winget install mozilla.firefox
 
 #### Development
-winget install --id JetBrains.Toolbox
-winget install --id AdoptOpenJDK.OpenJDK.11
-winget install --id Python.Python.3
 winget install --id Git.Git
 winget install --id Microsoft.VisualStudioCode
 winget install --id Alacritty.Alacritty
-# winget install --id OpenJS.NodeJSLTS
-# winget install --id Oracle.VirtualBox
-# winget install --id mitmproxy
-# winget install --id OBSProject.OBSStudio
-
-# timezone
-tzutil /s "Georgian Standard Time"
 
 # git stuff
 git config --global user.email "shalva258@gmail.com"
 git config --global user.name "shalva97"
 
-# Import Registry files in '.\reg' folder
-foreach ($reg in (Get-ChildItem -Path ".\reg\*" -Include @("*.reg")).FullName) {
-    reg.exe import $reg
-}
